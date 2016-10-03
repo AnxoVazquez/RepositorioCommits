b4e86b46a89c36ebb3a9fd013d51e57ce178ddfb
avazquezsuarez <avazquezsuarez@victoria15.danielcastelao.org>
2016-10-03 09:47:25 +0200

    Inicio del proyecto

---
+++ src/rectanguloarea/RectanguloArea.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rectanguloarea;
+
+/**
+ *
+ * @author avazquezsuarez
+ */
+public class RectanguloArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int base = 45;
+        int altura = 79;
+        double area;
+        area= base*altura;
+        System.out.print("El área de un rectángulo de base "+ base + " y altura "+ altura +" es: "+area);
+                
+    }
+    
+}
